bf03f51c05558932c2e533152c158483cc743b85
xlizer1 <mustafa_142@mail.ru>
2024-10-16 23:48:47 +0300

    save

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("./src/config/db");
 const http = require("http");
 const app = require("./app");
 const mqtt = require("mqtt");
-const socketIo = require('socket.io');
+const socketIo = require("socket.io");
 const port =
   process.env.SERVER_PORT === undefined ? 3001 : process.env.SERVER_PORT;
 const ip =
@@ -15,11 +15,31 @@ const io = socketIo(server, {
     origin: "http://localhost:3000", // Update this with your React app's URL
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
 const mqttClient = mqtt.connect("mqtt://13.48.192.48:1883");
+
+function generateSensorData() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  const temperature = "28.39 C";
+  const heartRate = "0 bpm";
+  const spo2 = "0 %";
+
+  const sensorData = `start time: ${timeString} Temperature: ${temperature}, Heart Rate: ${heartRate}, SpO2: ${spo2}`;
+  return sensorData;
+}
+
+setInterval(() => {
+  const topic = "sensor/data"; // Replace with the actual topic if needed
+  io.emit("mqtt_message", { topic, message: generateSensorData() });
+}, 5000); // 5000 milliseconds = 5 seconds
 
 mqttClient.on("connect", () => {
   console.log("Connected to MQTT broker");
@@ -36,7 +56,7 @@ io.on("connection", (socket) => {
 
   socket.on("send_mqtt_message", (data) => {
     const { topic, message } = data;
-    console.log(message)
+    console.log(message);
     mqttClient.publish(topic, message);
   });
 
--- src/config/db.js
+++ src/config/db.js
@@ -1,47 +1,48 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const host = process.env.HOST === undefined ? 'localhost':process.env.HOST;
-const user = process.env.DB_USER === undefined ? 'root':process.env.DB_USER;
-const password = process.env.PASSWORD === undefined ? '' : process.env.PASSWORD;
-const database = process.env.DATABASE === undefined ? 'care_pulse':process.env.DATABASE;
+const host = process.env.HOST === undefined ? "localhost" : process.env.HOST;
+const user = process.env.DB_USER === undefined ? "root" : process.env.DB_USER;
+const password = process.env.PASSWORD === undefined ? "" : process.env.PASSWORD;
+const database =
+  process.env.DATABASE === undefined ? "care_pulse" : process.env.DATABASE;
 
 let con;
 
 /* db connection with retry */
 function connectWithRetry() {
-  console.log('Trying to connect to the DB');
+  console.log("Trying to connect to the DB");
   con = mysql.createConnection({
     host: host,
     user: user,
     password: password,
     database: database,
     multipleStatements: true,
-    charset: 'utf8',
+    charset: "utf8",
     queryTimeout: 5000, // 5 seconds query timeout,
-    timezone:'Asia/Baghdad',
+    timezone: "Asia/Baghdad",
   });
 
   con.connect((err) => {
     if (err) {
-      console.error('Error in DB connection:', err);
-      console.log('Retrying DB connection in 5 seconds...');
+      console.error("Error in DB connection:", err);
+      console.log("Retrying DB connection in 5 seconds...");
 
       // Retry the connection after a delay
       setTimeout(connectWithRetry, 5000);
     } else {
-      console.log('DB Connected to:', database);
+      console.log("DB Connected to:", database);
     }
   });
 
-  con.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection lost');
-      console.log('Retrying DB connection in 5 seconds...');
+  con.on("error", (err) => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection lost");
+      console.log("Retrying DB connection in 5 seconds...");
 
       // Retry the connection after a delay
       setTimeout(connectWithRetry, 5000);
     } else {
-      console.error('Fatal error encountered, creating a new DB connection');
+      console.error("Fatal error encountered, creating a new DB connection");
       con.destroy(); // Destroy the existing connection
 
       // Retry the connection immediately
@@ -84,18 +85,18 @@ connectWithRetry();
 //   })
 // }, queryTimeout);
 
-module.exports.mysqlConnection = con
+module.exports.mysqlConnection = con;
 module.exports.query = function (...args) {
-  if (con && con.state === 'authenticated') {
+  if (con && con.state === "authenticated") {
     con.query(...args, (err, results) => {
       if (err) {
-        console.error('Error executing query:', err);
+        console.error("Error executing query:", err);
       } else {
-        console.log('Query executed successfully:', results);
+        console.log("Query executed successfully:", results);
       }
     });
   } else {
-    console.error('Cannot execute query, no valid DB connection available');
+    console.error("Cannot execute query, no valid DB connection available");
     connectWithRetry();
   }
 };
